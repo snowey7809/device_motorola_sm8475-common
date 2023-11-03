b6aab72803814a47c82bfe4edfeb76d45f510f68
Nishant Kumar <www.rajsonu13@gmail.com>
2023-11-03 13:06:20 +0530

    sm8475-common: Drop prebuilt display config lib

--- sm8475.mk
+++ sm8475.mk
@@ -171,6 +171,7 @@ PRODUCT_PACKAGES += \
     vendor.display.config@2.0.vendor \
     vendor.qti.hardware.display.config \
     vendor.qti.hardware.display.config-V2-ndk.vendor \
+    vendor.qti.hardware.display.config-V5-ndk.vendor \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.composer-service \
     vendor.qti.hardware.display.demura-service \
