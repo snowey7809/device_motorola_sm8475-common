1f9c90b24eba66ef1be74302586a4bc2a0424868
Nishant Kumar <www.rajsonu13@gmail.com>
2023-11-04 14:51:14 +0530

    sm8475-common: Build libfmpal
    * 11-04 14:36:27.694  1611  1611 E AHAL: AudioExtn: audio_extn_fm_init: 796: dlopen failed with: dlopen failed: library "/vendor/lib64/libfmpal.so" not found

--- sm8475.mk
+++ sm8475.mk
@@ -113,7 +113,8 @@ PRODUCT_PACKAGES += \
     libsndcardparser \
     vendor.qti.hardware.AGMIPC@1.0-service \
     vendor.qti.hardware.pal@1.0-impl \
-    libtinycompress
+    libtinycompress \
+    libfmpal
 
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration_7_0.xml \
