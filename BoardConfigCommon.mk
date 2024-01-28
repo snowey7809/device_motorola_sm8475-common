763d57704528336743ea8243457e3624eed06310
Nishant Kumar <www.rajsonu13@gmail.com>
2024-01-28 12:31:41 +0530

    sm8475-common: Drop ndk platform backend flag

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -18,9 +18,6 @@ BOARD_VENDOR := motorola
 
 COMMON_PATH := device/motorola/sm8475-common
 
-# Broken
-NEED_AIDL_NDK_PLATFORM_BACKEND := true
-
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv9-a
