8fab9296db719b153ab60fcd9a7e9fd45ae58a84
DillerOFire <niktofe1@gmail.com>
2024-02-05 13:37:38 +0530

    sm8475-common: Switch to 64Bit only
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -67,27 +67,16 @@ function blob_fixup() {
         system_ext/etc/permissions/moto-telephony.xml)
             sed -i "s#/system/#/system_ext/#" "${2}"
             ;;
-        vendor/etc/media_cape/video_system_specs.json \
-        |vendor/etc/media_ukee/video_system_specs.json \
-        |vendor/etc/media_taro/video_system_specs.json)
+        vendor/etc/media_cape/video_system_specs.json)
             sed -i "/max_retry_alloc_output_timeout/ s/2000/0/" "${2}"
             ;;
         vendor/etc/vintf/manifest/vendor.dolby.media.c2@1.0-service.xml)
             sed -ni '/default1/!p' "${2}"
             ;;
-        vendor/lib/libcamximageformatutils.so)
-            ${PATCHELF} --replace-needed "vendor.qti.hardware.display.config-V2-ndk_platform.so" "vendor.qti.hardware.display.config-V2-ndk.so" "${2}"
-            ;;
         vendor/lib64/libcamximageformatutils.so)
             ${PATCHELF} --replace-needed "vendor.qti.hardware.display.config-V2-ndk_platform.so" "vendor.qti.hardware.display.config-V2-ndk.so" "${2}"
             ;;
         vendor/bin/hw/android.hardware.security.keymint-service-qti)
-            ${PATCHELF} --replace-needed "android.hardware.security.keymint-V1-ndk_platform.so" "android.hardware.security.keymint-V1-ndk.so" "${2}"
-            ${PATCHELF} --replace-needed "android.hardware.security.secureclock-V1-ndk_platform.so" "android.hardware.security.secureclock-V1-ndk.so" "${2}"
-            ${PATCHELF} --replace-needed "android.hardware.security.sharedsecret-V1-ndk_platform.so" "android.hardware.security.sharedsecret-V1-ndk.so" "${2}"
-            ${PATCHELF} --add-needed "android.hardware.security.rkp-V1-ndk.so" "${2}"
-            ;;
-        vendor/lib/libqtikeymint.so)
             ${PATCHELF} --replace-needed "android.hardware.security.keymint-V1-ndk_platform.so" "android.hardware.security.keymint-V1-ndk.so" "${2}"
             ${PATCHELF} --replace-needed "android.hardware.security.secureclock-V1-ndk_platform.so" "android.hardware.security.secureclock-V1-ndk.so" "${2}"
             ${PATCHELF} --replace-needed "android.hardware.security.sharedsecret-V1-ndk_platform.so" "android.hardware.security.sharedsecret-V1-ndk.so" "${2}"
@@ -106,7 +95,7 @@ function blob_fixup() {
             sed -i "s#build_codename -le \"12\"#build_codename -le \"13\"#" "${2}"
             ;;
         # rename moto modified primary audio to not conflict with source built
-        vendor/lib/hw/audio.primary.taro-moto.so | vendor/lib64/hw/audio.primary.taro-moto.so)
+        vendor/lib64/hw/audio.primary.taro-moto.so)
             "${PATCHELF}" --set-soname audio.primary.taro-moto.so "${2}"
             ;;
         vendor/lib64/vendor.qti.gnss-service.so)
